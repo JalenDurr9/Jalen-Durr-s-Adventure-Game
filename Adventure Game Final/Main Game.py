b9bb9cec4e78559fec088438048880bad7a8ab1a
JalenDurr9 <77880306+JalenDurr9@users.noreply.github.com>
2021-03-21 21:18:34 -0500

    Add files via upload

---
+++ Adventure Game Final/Main Game.py
@@ -0,0 +1,113 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import time
+>>> print("Welcome to the Start of Chapter One.")
+Welcome to the Start of Chapter One.
+>>> print("Complete the task to repair the ship and go home.")
+Complete the task to repair the ship and go home.
+>>> def parkinglot():
+	globalvariables.playeraction = input("Task 1: Find the tools to fix the engine of the spaceship.")
+	globalvariables.playeraction = input("Task 2: Check the ship to see if any crew members are alive.")
+	globalvariables.playeraction = input("Task 3: Find parts to build the wonder weapon to fight the alien attack.")
+
+	
+>>> chapteranswer = input("Did you complete all the tasks of the chapter? Yes/No")
+Did you complete all the tasks of the chapter? Yes/No
+>>> if chapteranswer == "Yes":
+	print("On to the next chapter.")
+elif chapteranswer == "No":
+	print("restart the current chapter.")
+else:
+	print("Quit and head to the main menu.")
+
+	
+Quit and head to the main menu.
+>>> import time
+>>> print("Welcome to the Start of Chapter Two.")
+Welcome to the Start of Chapter Two.
+>>> print("Complete the tasks to repair the ship and go home.")
+Complete the tasks to repair the ship and go home.
+>>> def parkinglot():
+	globalvariables.playeraction = input("Task 1: Repair the windows to prevent aliens from boarding the ship.")
+	globalvariables.playeraction = input("Task 2: Start the repair of the engine.")
+	globalvariables.playeraction = input("Task 3: Find parts to build a shield to protect you if there is a attack.")
+
+	
+>>> chapteranswer = input("Did you complete all the tasks of the chapter? Yes/No")
+Did you complete all the tasks of the chapter? Yes/No
+>>> if chapteranswer == "Yes":
+	print("On to the next chapter.")
+elif chapteranswer == "No":
+	print("restart the previous chapter.")
+else:
+	print("Quit and head to the main menu.")
+
+	
+Quit and head to the main menu.
+>>> import time
+>>> print("Welcome to the Start of Chapter Three.")
+Welcome to the Start of Chapter Three.
+>>> print("Complete the task to repair the ship and go home.")
+Complete the task to repair the ship and go home.
+>>> def parkinglot():
+	globalvariables.playeraction = input("Task 1: Start building the shield to protect the character from the ailens.")
+	globalvariables.playeraction = input("Task 2: Start building the wonder weapon.")
+	globalvariables.playeraction = input("Task 3: Fight off the alien but be mindful of your health bar.")
+
+	
+>>> chapteranswer = input("Did you complete all the tasks of the chapter? Yes/No")
+Did you complete all the tasks of the chapter? Yes/No
+>>> if chapteranswer == "Yes":
+	print("On to the next chapter.")
+elif chapteranswer == "No":
+	print("restart the previous chapter.")
+else:
+	print("Quit and head to the main menu.")
+
+	
+Quit and head to the main menu.
+>>> import time
+>>> print("Welcome to the Start of Chapter Four.")
+Welcome to the Start of Chapter Four.
+>>> print("Complete the task to repair the ship and go home.")
+Complete the task to repair the ship and go home.
+>>> def parkinglot():
+	globalvariables.playeraction = input("Task 1: Fix the Electricity.")
+	globalvariables.playeraction = input("Task 2: Repair the communication systems.")
+	globalvariables.playeraction = input("Task 3: Build another shield because the last one broke due to wear and tear.")
+	globalvariables.playeraction = input("Task 4: Find ammo on board the ship to fight the last wave of aliens.")
+
+	
+>>> chapteranswer = input("Did you complete all the tasks of the chapter? Yes/No")
+Did you complete all the tasks of the chapter? Yes/No
+>>> if chapteranswer == "Yes":
+	print("On to the next chapter.")
+elif chapteranswer == "No":
+	print("restart the previous chapter.")
+else:
+	print("Quit and head to the main menu.")
+
+	
+Quit and head to the main menu.
+>>> import time
+>>> print("Welcome to the Start of Chapter Five.")
+Welcome to the Start of Chapter Five.
+>>> print("Complete the last tasks and be on your way home.")
+Complete the last tasks and be on your way home.
+>>> def parkinglot():
+	globalvariables.playeraction = input("Task 1: Use the wonder weapon and the ship's gun to fight the last horde of aliens.")
+	globalvariables.playeraction = input("Task 2: Fight that last horde of aliens in their ships so that you can take off and go back to your original planet.")
+
+	
+>>> chapteranswer = input("Did you complete all the tasks of the chapter? Yes/No")
+Did you complete all the tasks of the chapter? Yes/No
+>>> if chapteranswer == "Yes":
+	print("CONGRATULATIONS! You have completed the game!!!.")
+elif chapteranswer == "No":
+	print("restart the previous chapter.")
+else:
+	print("Quit and head to the main menu.")
+
+	
+Quit and head to the main menu.
+>>> 
---
+++ Adventure Game Final/Start Game.py
@@ -0,0 +1,21 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import time
+>>> def parkinglot():
+	globalvariables.characterselect = input("Welcome To Area 51. Please Select your Character: ")
+	globalvariables.color = input("Please Select the color of your Character: ")
+	globalvariables.vehicle = input("Please the type of ship you want to explore in: ")
+	globalvariables.difficulty = int(input("Please select what difficulty you want to play on"))
+
+	
+>>> beginanswer = input("Would you like to start tutorial? Yes/No")
+Would you like to start tutorial? Yes/No
+>>> if beginanswer == "Yes":
+	print("Let's start the tutorial missions")
+
+	
+>>> if beginanswer == "No":
+	print("Let's move on to the real game")
+
+	
+>>> 
